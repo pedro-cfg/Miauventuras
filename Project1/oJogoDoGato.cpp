8425879323c7ef608669c4509e7fe9f512001f7f
pedro-cfg <pedro.cfguimaraes@gmail.com>
2021-11-05 11:25:57 -0300

    hello world

---
+++ Project1/oJogoDoGato.cpp
@@ -0,0 +1,6 @@
+#include "stdafx.h"
+
+int main() {
+	cout << "HELLO WORLD" << endl;
+	return 0;
+}
---
+++ Project1/stdafx.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+#include <iostream>
+using namespace std;
+
+ 
